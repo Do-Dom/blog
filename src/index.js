8f3576ff53d463742b032b1ff43103ce79a72464
Do-Dom <yunjeong.syj@gmail.com>
2021-01-26 16:02:08 +0900

    make posting system

--- src/index.js
+++ src/index.js
@@ -1,17 +1,159 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Title extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    const title = this.props.title;
+    return (
+      <div className="title-container">
+        [ {title} ]
+      </div>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Context extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    const context = this.props.context;
+    return (
+      <div className="context-container">
+        {context}  
+      </div>
+    );
+  }
+}
+
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+ 
+  render() {
+    return (
+      <fieldset className="post-field-container">
+        <legend>
+          <Title title={this.props.title} />
+        </legend>        
+        <span> Posted Time : {this.props.time} </span>
+        <Context context = {this.props.context} />
+      </fieldset>
+    );
+  }
+}
+
+class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit}>
+        <input 
+          type = "text" 
+          onChange={this.props.handleChange}
+          name = "title"/>
+        <br/>
+        <textarea 
+          type = "text"
+          onChange={this.props.handleChange}
+          name = "context"/>
+        <br/>
+        <input type = "submit" value = "POST!" />
+      </form>
+    );
+  }
+}
+
+class PostCreator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = (event) => {
+    this.setState(
+      {[event.target.name]: event.target.value});
+  }
+  
+  handleSubmit = (event) => {
+    event.preventDefault();
+    
+    this.setState(
+      {time:new Date().toLocaleTimeString()}, ()=>{
+        const post = 
+              <Post key = {this.state.time}
+                title = {this.state.title}
+                context = {this.state.context}
+                time = {this.state.time}
+              />;
+        
+        this.props.handleNewPost(post);
+       }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <PostForm 
+          handleChange = {this.handleChange}
+          handleSubmit = {this.handleSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNewPost = this.handleNewPost.bind(this);
+    this.state = {
+      postList : []
+    };
+  }
+
+  handleNewPost (data) {
+    console.log(data);
+    const postList = this.state.postList;
+    this.setState(
+      {postList: postList.concat([data]) }
+    );
+  }
+  
+  render() {
+    return (
+      <div>
+        <PostCreator 
+          handleNewPost = {this.handleNewPost}
+        />
+        {this.state.postList}
+      </div>
+    );
+  }
+  
+  
+}
+
+// ReactDOM.render(
+//   <div>
+//     <Blog />
+//   </div>,
+//   document.getElementById("app")
+// );
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    ReactDOM.render(
+        <div>
+            <Blog />
+        </div>,
+        document.getElementById('app'));
+});
